97604a6d45e32358fc47dfb98341415cec6804cd
James R <jimmer1609@gmail.com>
2022-08-25 11:00:05 +0900

    order form input fields added

--- src/components/orderForm/OrderForm.js
+++ src/components/orderForm/OrderForm.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 
 function OrderForm() {
+
+  const handleItemSubmit = (evt) => {}
+
   return (
-    <h2>Order Form component</h2>
+    <>
+      <h2>Order Form component</h2>
+      <form>
+        <label>
+          Item:
+          <select>
+            <option value="hamburger">Hamburger</option>
+            <option value="fries">Fries</option>
+            <option value="ribeye steak">RibEye Steak</option>
+            <option value="chicken alfredo fettucine">Chicken Alfredo Fettucine</option>
+          </select>
+        </label>
+        <label>
+          Quantity:
+          <select>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </label>
+        <label>
+          Table No.:
+          <input type="text" required/>
+        </label>
+        <label>
+          Note:
+          <textarea />
+        </label>
+        <input type="submit" value="Submit"></input>
+
+      </form>
+    </>
   )
 }
 
