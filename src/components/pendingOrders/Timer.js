84540d9e0474cea0c2259222ce4964d079398e1c
James R <jimmer1609@gmail.com>
2022-08-25 13:25:53 +0900

    timer component added

---
+++ src/components/pendingOrders/Timer.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from "react";
+
+function Timer() {
+const [time, setTime] = useState(0);
+const [timerOn, setTimerOn] = useState(false);
+
+useEffect(() => {
+    let interval = null;
+
+    if (timerOn) {
+    interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10)
+    }, 10)
+    } else {
+    clearInterval(interval)
+    }
+
+    return () => clearInterval(interval)
+
+}, [timerOn])
+
+return (
+    <>
+    <div>
+        <span>{("0" + Math.floor(time / 1000) % 60).slice(-2)}:</span>
+        <span>{("0" + (time / 10)).slice(-2)}</span>
+    </div>
+    </>
+  )
+}
+
+export default Timer;
